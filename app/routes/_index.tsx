ef8be51b8378d5bebbf1f9917e0b7f2564cef9bb
MatheusAVZ <matheusavaz12@gmail.com>
2025-01-23 22:36:36 -0300

    adds favicon

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -39,7 +39,7 @@ const products = [
 
 export default function Index() {
   return (
-    <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col bg-white">
       <Header />
 
       <main className="flex-grow">
