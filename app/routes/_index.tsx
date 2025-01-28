688a41886be1441875d963efb1716fb2e427f14a
MatheusAVZ <matheusavaz12@gmail.com>
2025-01-28 00:20:59 -0300

    fix: adjust carousel text

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -18,25 +18,37 @@ export default function Index() {
           <ProductCarousel />
         </section>
 
-        <section className="relative flex flex-1">
-          <ImageCarousel />
-          <div className="absolute left-0 right-0 top-0 flex h-1/2 w-full" />
-          <div className="absolute bottom-0 left-0 right-0 flex h-1/2 flex-col items-center justify-end bg-gradient-to-t from-white to-transparent p-10">
-            <p className="text-center text-xl">
+        <section className="relative flex flex-1 flex-col gap-2">
+          <div className="flex flex-col gap-2 md:hidden">
+            <p className="text-center text-sm font-semibold">
               A Clanap Comércio Importação e Exportação, desde 1997 vem
               representando, exclusivamente em todo o Brasil, uma gama de
               produtos de ponta para a área da Construção Civil.
             </p>
-            <p className="text-center text-xl">
+            <p className="text-center text-sm font-semibold">
               As inovações, são visíveis no resultado final dos produtos que
               oferece, fruto de uma busca incessante de novas marcas com
               qualidade, praticidade e elegância.
             </p>
-            <div className="mt-8">
-              <Button variant="primary" className="px-8 text-lg">
-                QUEM SOMOS
-              </Button>
+          </div>
+          <ImageCarousel />
+          <div className="absolute left-0 right-0 top-0 flex h-1/3 w-full" />
+          <div className="absolute bottom-0 left-0 right-0 flex h-2/3 flex-col items-center justify-end bg-gradient-to-t from-white to-transparent p-10">
+            <div className="hidden flex-col gap-2 md:flex">
+              <p className="text-center font-semibold">
+                A Clanap Comércio Importação e Exportação, desde 1997 vem
+                representando, exclusivamente em todo o Brasil, uma gama de
+                produtos de ponta para a área da Construção Civil.
+              </p>
+              <p className="text-center font-semibold">
+                As inovações, são visíveis no resultado final dos produtos que
+                oferece, fruto de uma busca incessante de novas marcas com
+                qualidade, praticidade e elegância.
+              </p>
             </div>
+            <Button variant="primary" className="mt-8 px-8 text-lg">
+              QUEM SOMOS
+            </Button>
           </div>
         </section>
       </main>
