4951f77f7df73930e0d5d16dd58b5831d7abdb21
MatheusAVZ <matheusavaz12@gmail.com>
2025-01-27 22:54:54 -0300

    feat: adds animation to product card

--- app/containers/product-card.tsx
+++ app/containers/product-card.tsx
@@ -1,4 +1,5 @@
 import { Button } from "~/components/button"
+import { motion } from 'framer-motion';
 
 interface ProductCardProps {
   title: string
@@ -15,9 +16,15 @@ export function ProductCard({ title, description, image }: ProductCardProps) {
       <div className="p-6">
         <h3 className="text-lg font-semibold mb-2 text-orange-500">{title}</h3>
         <p className="text-gray-600 text-sm mb-4">{description}</p>
-        <Button variant="text" className="w-full bg-orange-500 hover:bg-orange-600">
-          SAIBA MAIS
-        </Button>
+        <motion.div
+          initial={{ scale: 1 }}
+          transition={{ duration: 0.4 }}
+          whileHover={{scale: 1.07}}
+        >
+          <Button variant="text" className="w-full bg-orange-500 hover:bg-orange-600">
+            SAIBA MAIS
+          </Button>
+        </motion.div>
       </div>
     </div>
   )
